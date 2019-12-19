e00281200e35886f3a5738c5e1e5a8f09422ef0c
ath92 <tom@ath92.nl>
2019-12-19 11:18:30 +0100

    change transform origin on some things, remove wall rotations

--- src/components/World/World.tsx
+++ src/components/World/World.tsx
@@ -1,26 +1,17 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import Wall from '../Wall/Wall';
-import useTick from '../../hooks/useTick';
 import { vec3 } from 'gl-matrix';
 import styles from './world.module.css';
 
-const World: React.FC = (props) => {
-    const [wallRotation, setWallRotation] = useState(0);
-
-    const updateWallRotation = useCallback(() => {
-        setWallRotation(w => (w + 0.05));
-    }, [setWallRotation]);
-    useTick(updateWallRotation);
-
-    return (
-        <>
-            <div className={styles.floor}></div>
-            <Wall position={vec3.fromValues(100, 0, 0)} yRotation={wallRotation}>
-                <Wall position={vec3.fromValues(0, 0, 0)} yRotation={100*Math.sin(wallRotation / 100)}>
-                </Wall>
-            </Wall>
-        </>
-    );
+const World: React.FC = props => {
+	return (
+		<>
+			<div className={styles.floor} />
+			<Wall position={vec3.fromValues(1000, 0, 0)} yRotation={0}>
+				<Wall position={vec3.fromValues(0, 0, 0)} yRotation={0.5 * Math.PI} />
+			</Wall>
+		</>
+	);
 };
 
 export default World;
